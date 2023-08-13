7b681450bb694d5ccc72da1957ac5803d6b68744
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-13 21:24:20 +0000

    sub

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 
+import { useState } from 'react';
 import React from "react";
 import './../styles/App.css';
-
+import Tabs from './Tabs'
 const App = () => {
+  let tabs=[{titles:'1',content:'Tab 1'},{titles:'2',content:'Tab 2'},{titles:'3',content:'Tab 3'}]
+  let [tab,Settab]= useState('')
+  function changetab(data){
+    Settab(data)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
-        Hello I am ass
+        {<Tabs tabs={tabs} onclick={changetab}/>}
+        {tab && <div>This is the content for {tab}.</div>}
     </div>
   )
 }
