17057438ddcffd44d751c829a374b3303770451a
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-22 22:20:58 +0000

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,13 @@ import './../styles/App.css';
 import Tabs from './Tabs'
 const App = () => {
   let tabs=[{titles:'1',content:'Tab 1'},{titles:'2',content:'Tab 2'},{titles:'3',content:'Tab 3'}]
-  let [tab,Settab]= useState('')
+  let [tab,Settab]= useState('Tab 1')
   function changetab(data){
     Settab(data)
   }
   return (
     <div>
-        
+
         {<Tabs tabs={tabs} onclick={changetab}/>}
         {tab && <div>This is the content for {tab}.</div>}
     </div>
